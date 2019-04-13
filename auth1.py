c2a16bf8d2349b9700914ae6695ae3e019dc7eec
andyt530 <andyt530@outlook.com>
2019-04-13 16:48:13 +0100

    expand providers

--- auth1.py
+++ auth1.py
@@ -127,10 +127,192 @@ for j in range(0, count):
     elif rtype == "Microsoft.Network/networkSecurityGroups":
         prov="azurerm_network_security_group"
         fr.write(rg + ":" + prov + "\n")
-    #elif:
-    #elif:
-    #else:
-    np.write(prov + "\n" noprovider.txt
+
+    elif rtype =="Microsoft.Storage/storageAccounts": 
+        prov="azurerm_storage_account"
+        fr.write(rg + ":" + prov + "\n")
+        prov="azurerm_storage_share"
+        fr.write(rg + ":" + prov + "\n")
+        prov="azurerm_storage_container"
+        fr.write(rg + ":" + prov + "\n")
+
+    elif rtype == "Microsoft.Network/networkSecurityGroups":
+        prov="azurerm_network_security_group"
+        fr.write(rg + ":" + prov + "\n")
+
+    elif rtype == "Microsoft.Compute/virtualMachines": 
+        #echo $isext
+        if [ "$isext" != "extensions" ]; then
+            prov="azurerm_virtual_machine"
+            fr.write(rg + ":" + prov + "\n")
+        fi
+            
+    elif rtype == "Microsoft.Network/networkInterfaces": 
+        prov="azurerm_network_interface"
+        fr.write(rg + ":" + prov + "\n")
+    
+    elif rtype == "Microsoft.Compute/disks":
+        prov="azurerm_managed_disk"
+        fr.write(rg + ":" + prov + "\n")
+        
+    elif rtype == "Microsoft.Automation/automationAccounts": 
+        prov="azurerm_automation_account"
+        fr.write(rg + ":" + prov + "\n")
+          
+    elif rtype == "Microsoft.Network/virtualNetworks":
+        prov="azurerm_virtual_network"
+        fr.write(rg + ":" + prov + "\n")
+        prov="azurerm_subnet"
+        fr.write(rg + ":" + prov + "\n")
+        prov="azurerm_virtual_network_peering"
+        fr.write(rg + ":" + prov + "\n")
+
+    elif rtype == "Microsoft.Network/publicIPAddresses":
+        prov="azurerm_public_ip"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.Network/loadBalancers":
+        prov="azurerm_lb"
+        fr.write(rg + ":" + prov + "\n")
+        prov="azurerm_lb_nat_rule"
+        fr.write(rg + ":" + prov + "\n")
+        prov="azurerm_lb_nat_pool"
+        fr.write(rg + ":" + prov + "\n")
+        prov="azurerm_lb_backend_address_pool"
+        fr.write(rg + ":" + prov + "\n")
+        prov="azurerm_lb_probe"
+        fr.write(rg + ":" + prov + "\n")
+        prov="azurerm_lb_rule"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.Network/virtualNetworkGateways":
+        prov="azurerm_virtual_network_gateway"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.Network/connections":
+        prov="azurerm_virtual_network_gateway_connection"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.Network/routeTables": 
+        prov="azurerm_route_table"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.OperationalInsights/workspaces":
+        prov="azurerm_log_analytics_workspace"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype ==  "Microsoft.OperationsManagement/solutions":
+        prov="azurerm_log_analytics_solution"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.KeyVault/vaults":
+        prov="azurerm_key_vault"
+        fr.write(rg + ":" + prov + "\n")
+        prov="azurerm_key_vault_secret"
+        fr.write(rg + ":" + prov + "\n")
+
+    elif rtype == "Microsoft.RecoveryServices/vaults":
+        prov="azurerm_recovery_services_vault"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.ContainerRegistry/registries":
+        prov="azurerm_container_registry"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.ContainerService/managedClusters":
+        prov="azurerm_kubernetes_cluster"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.Network/localNetworkGateways":
+        prov="azurerm_local_network_gateway"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.Network/expressRouteCircuits":
+        prov="azurerm_express_route_circuit"
+        fr.write(rg + ":" + prov + "\n")
+        prov="azurerm_express_route_circuit_authorization"
+        fr.write(rg + ":" + prov + "\n")
+        prov="azurerm_express_route_circuit_peering"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.Compute/images": 
+        prov="azurerm_image"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.Network/networkWatchers": 
+        prov="azurerm_network_watcher"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.Network/applicationSecurityGroups":
+         prov="azurerm_application_security_group"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.ContainerInstance/containerGroups":
+         prov="azurerm_container_group"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.Network/applicationGateways": 
+        prov="azurerm_application_gateway"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.DocumentDb/databaseAccounts":
+        prov="azurerm_cosmosdb_account"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.ServiceBus/namespaces": 
+        prov="azurerm_servicebus_namespace"
+        fr.write(rg + ":" + prov + "\n")
+        prov="azurerm_servicebus_queue"
+        fr.write(rg + ":" + prov + "\n")
+                  
+    elif rtype == "Microsoft.Network/trafficmanagerprofiles":
+        prov="azurerm_traffic_manager_profile"
+        fr.write(rg + ":" + prov + "\n")
+        prov="azurerm_traffic_manager_endpoint"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.Web/serverFarms": 
+        prov="azurerm_app_service_plan"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.Web/sites": 
+        prov="azurerm_app_service"
+        fr.write(rg + ":" + prov + "\n")
+        prov="azurerm_function_app"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.Compute/virtualMachineScaleSets":
+        prov="azurerm_virtual_machine_scale_set"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.ManagedIdentity/userAssignedIdentities":
+        prov="azurerm_user_assigned_identity"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.Compute/snapshots":
+        prov="azurerm_snapshot"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.Databricks/workspaces":
+        prov="azurerm_databricks_workspace"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.Sql/servers": 
+        prov="azurerm_sql_server"
+        fr.write(rg + ":" + prov + "\n")
+        prov="azurerm_sql_database"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype == "Microsoft.Network/dnszones": 
+        prov="azurerm_dns_zone"
+        fr.write(rg + ":" + prov + "\n")
+            
+    elif rtype ==  "microsoft.insights/autoscalesettings":
+        prov="azurerm_monitor_autoscale_setting"
+        fr.write(rg + ":" + prov + "\n")
+            
+    else:
+        np.write(prov + "\n" noprovider.txt
 
 fr.close()
 np.close()
