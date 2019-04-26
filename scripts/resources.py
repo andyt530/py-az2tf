1ad04743115214736374a457eaabfc51f96fcc27
andyt530 <andyt530@outlook.com>
2019-04-26 20:16:15 +0100

    tm profile

--- scripts/resources.py
+++ scripts/resources.py
@@ -449,147 +449,147 @@ tfim.close()
 
 tfp="azurerm_management_lock"
 azr=""
+if crf in tfp:
+    p = subprocess.Popen('az lock list -o json', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output, errors = p.communicate()
+    azr=json.loads(output)
 
-p = subprocess.Popen('az lock list -o json', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-output, errors = p.communicate()
-azr=json.loads(output)
+    if cde:
+        print(json.dumps(azr, indent=4, separators=(',', ': ')))
 
-if cde:
-    print(json.dumps(azr, indent=4, separators=(',', ': ')))
-
-tfrmf="002-"+tfp+"-staterm.sh"
-tfimf="002-"+tfp+"-stateimp.sh"
-tfrm=open(tfrmf, 'a')
-tfim=open(tfimf, 'a')
-print tfp,
-count=len(azr)
-print count
-for j in range(0, count):
-    
-    name=azr[j]["name"]
-    #loc=azr[j]["location"]
-    id=azr[j]["id"]
-    rg=azr[j]["resourceGroup"]
-    level=azr[j]["level"]
-    notes=azr[j]["notes"]
-    scope1=id.split("/Microsoft.Authorization")[0].rstrip("providers")
-    scope=scope1.rstrip("/")
+    tfrmf="002-"+tfp+"-staterm.sh"
+    tfimf="002-"+tfp+"-stateimp.sh"
+    tfrm=open(tfrmf, 'a')
+    tfim=open(tfimf, 'a')
+    print tfp,
+    count=len(azr)
+    print count
+    for j in range(0, count):
+        
+        name=azr[j]["name"]
+        #loc=azr[j]["location"]
+        id=azr[j]["id"]
+        rg=azr[j]["resourceGroup"]
+        level=azr[j]["level"]
+        notes=azr[j]["notes"]
+        scope1=id.split("/Microsoft.Authorization")[0].rstrip("providers")
+        scope=scope1.rstrip("/")
 
 
-    if crg is not None:
-        print "rgname=" + rg + " crg=" + crg
-        if rg.lower() != crg.lower():
-            continue  # back to for
-    
+        if crg is not None:
+            print "rgname=" + rg + " crg=" + crg
+            if rg.lower() != crg.lower():
+                continue  # back to for
+        
 
-    rname=name.replace(".","-")
-    prefix=tfp+"."+rg+'__'+rname
-    #print prefix
-    rfilename=prefix+".tf"
-    fr=open(rfilename, 'w')
-    fr.write("")
-    fr.write('resource ' + tfp + ' ' + rg + '__' + rname + ' {\n')
-    fr.write('\t name = "' + name + '"\n')
-    #fr.write('\t location = "'+ loc + '"\n')
-    fr.write('\t lock_level = "'+ level + '"\n')   
-    fr.write('\t notes = "'+ notes + '"\n') 
-    fr.write('\t scope = "'+ scope + '"\n')
-# tags block
-    try:
-        mtags=azr[j]["tags"]
-    except:
-        mtags="{}"
-    tcount=len(mtags)-1
-    if tcount > 1 :
-        fr.write('tags { \n')
-        print tcount
-        for key in mtags.keys():
-            tval=mtags[key]
-            fr.write('\t "' + key + '"="' + tval + '"\n')
-        #print(json.dumps(mtags, indent=4, separators=(',', ': ')))
-        fr.write('}\n')
-    
-    fr.write('}\n') 
-    fr.close()  # close .tf file
+        rname=name.replace(".","-")
+        prefix=tfp+"."+rg+'__'+rname
+        #print prefix
+        rfilename=prefix+".tf"
+        fr=open(rfilename, 'w')
+        fr.write("")
+        fr.write('resource ' + tfp + ' ' + rg + '__' + rname + ' {\n')
+        fr.write('\t name = "' + name + '"\n')
+        #fr.write('\t location = "'+ loc + '"\n')
+        fr.write('\t lock_level = "'+ level + '"\n')   
+        fr.write('\t notes = "'+ notes + '"\n') 
+        fr.write('\t scope = "'+ scope + '"\n')
+    # tags block
+        try:
+            mtags=azr[j]["tags"]
+        except:
+            mtags="{}"
+        tcount=len(mtags)-1
+        if tcount > 1 :
+            fr.write('tags { \n')
+            print tcount
+            for key in mtags.keys():
+                tval=mtags[key]
+                fr.write('\t "' + key + '"="' + tval + '"\n')
+            #print(json.dumps(mtags, indent=4, separators=(',', ': ')))
+            fr.write('}\n')
+        
+        fr.write('}\n') 
+        fr.close()  # close .tf file
 
-    tfrm.write('terraform state rm '+tfp+'.'+rg+'__'+rname + '\n')
-    
-    tfcomm='terraform import '+tfp+'.'+rg+'__'+rname+' '+id+'\n'
-    tfim.write('echo "importing ' + str(j) + ' of ' + str(count-1) + '"' + '\n')
-    tfim.write(tfcomm)  
+        tfrm.write('terraform state rm '+tfp+'.'+rg+'__'+rname + '\n')
+        
+        tfcomm='terraform import '+tfp+'.'+rg+'__'+rname+' '+id+'\n'
+        tfim.write('echo "importing ' + str(j) + ' of ' + str(count-1) + '"' + '\n')
+        tfim.write(tfcomm)  
 
-# end for
-tfrm.close()
-tfim.close()
-#end management locks
+    # end for
+    tfrm.close()
+    tfim.close()
+    #end management locks
 
 # 015 user assigned identity
 tfp="azurerm_user_assigned_identity"
 azr=""
+if crf in tfp:
+    p = subprocess.Popen('az identity list -o json', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output, errors = p.communicate()
+    azr=json.loads(output)
+    if cde:
+        print(json.dumps(azr, indent=4, separators=(',', ': ')))
 
-p = subprocess.Popen('az identity list -o json', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-output, errors = p.communicate()
-azr=json.loads(output)
-if cde:
-    print(json.dumps(azr, indent=4, separators=(',', ': ')))
+    tfrmf="015-"+tfp+"-staterm.sh"
+    tfimf="015-"+tfp+"-stateimp.sh"
+    tfrm=open(tfrmf, 'a')
+    tfim=open(tfimf, 'a')
+    print tfp,
+    count=len(azr)
+    print count
+    for j in range(0, count):
+        
+        name=azr[j]["name"]
+        loc=azr[j]["location"]
+        id=azr[j]["id"]
+        rg=azr[j]["resourceGroup"]
 
-tfrmf="015-"+tfp+"-staterm.sh"
-tfimf="015-"+tfp+"-stateimp.sh"
-tfrm=open(tfrmf, 'a')
-tfim=open(tfimf, 'a')
-print tfp,
-count=len(azr)
-print count
-for j in range(0, count):
-    
-    name=azr[j]["name"]
-    loc=azr[j]["location"]
-    id=azr[j]["id"]
-    rg=azr[j]["resourceGroup"]
+        if crg is not None:
+            print "rgname=" + rg + " crg=" + crg
+            if rg.lower() != crg.lower():
+                continue  # back to for
+        
+        rname=name.replace(".","-")
+        prefix=tfp+"."+rg+'__'+rname
+        #print prefix
+        rfilename=prefix+".tf"
+        fr=open(rfilename, 'w')
+        fr.write("")
+        fr.write('resource ' + tfp + ' ' + rg + '__' + rname + ' {\n')
+        fr.write('\t name = "' + name + '"\n')
+        fr.write('\t location = "'+ loc + '"\n')
+        fr.write('\t resource_group_name = "' + rg + '"\n')
+    # tags block
+        try:
+            mtags=azr[j]["tags"]
+        except:
+            mtags="{}"
+        tcount=len(mtags)-1
+        if tcount > 1 :
+            fr.write('tags { \n')
+            print tcount
+            for key in mtags.keys():
+                tval=mtags[key]
+                fr.write('\t "' + key + '"="' + tval + '"\n')
+            #print(json.dumps(mtags, indent=4, separators=(',', ': ')))
+            fr.write('}\n')
+        
+        fr.write('}\n') 
+        fr.close()  # close .tf file
 
-    if crg is not None:
-        print "rgname=" + rg + " crg=" + crg
-        if rg.lower() != crg.lower():
-            continue  # back to for
-    
-    rname=name.replace(".","-")
-    prefix=tfp+"."+rg+'__'+rname
-    #print prefix
-    rfilename=prefix+".tf"
-    fr=open(rfilename, 'w')
-    fr.write("")
-    fr.write('resource ' + tfp + ' ' + rg + '__' + rname + ' {\n')
-    fr.write('\t name = "' + name + '"\n')
-    fr.write('\t location = "'+ loc + '"\n')
-    fr.write('\t resource_group_name = "' + rg + '"\n')
-# tags block
-    try:
-        mtags=azr[j]["tags"]
-    except:
-        mtags="{}"
-    tcount=len(mtags)-1
-    if tcount > 1 :
-        fr.write('tags { \n')
-        print tcount
-        for key in mtags.keys():
-            tval=mtags[key]
-            fr.write('\t "' + key + '"="' + tval + '"\n')
-        #print(json.dumps(mtags, indent=4, separators=(',', ': ')))
-        fr.write('}\n')
-    
-    fr.write('}\n') 
-    fr.close()  # close .tf file
+        tfrm.write('terraform state rm '+tfp+'.'+rg+'__'+rname + '\n')
+        
+        tfcomm='terraform import '+tfp+'.'+rg+'__'+rname+' '+id+'\n'
+        tfim.write('echo "importing ' + str(j) + ' of ' + str(count-1) + '"' + '\n')
+        tfim.write(tfcomm)  
 
-    tfrm.write('terraform state rm '+tfp+'.'+rg+'__'+rname + '\n')
-    
-    tfcomm='terraform import '+tfp+'.'+rg+'__'+rname+' '+id+'\n'
-    tfim.write('echo "importing ' + str(j) + ' of ' + str(count-1) + '"' + '\n')
-    tfim.write(tfcomm)  
-
-# end for
-tfrm.close()
-tfim.close()
-#end user assigned identity
+    # end for
+    tfrm.close()
+    tfim.close()
+    #end user assigned identity
 
 
 
@@ -1755,7 +1755,7 @@ if crf in tfp:
     tfrm.close()
     tfim.close()
 #end storage account
-cde=True
+
 tfp="azurerm_public_ip"
 azr=""
 if crf in tfp:
@@ -1849,7 +1849,118 @@ if crf in tfp:
     tfim.close()
 #end public ip
 
+#  124 Traffic manager profile
+cde=True
+tfp="azurerm_traffic_manager_profile"
+azr=""
+if crf in tfp:
+# REST or cli
+    print "REST Traffic Manager Profile"
+    url="https://management.azure.com/subscriptions/" + sub + "/providers/Microsoft.Network/trafficmanagerprofiles"
+    params = {'api-version': '2017-05-01'}
+    r = requests.get(url, headers=headers, params=params)
+    azr= r.json()["value"]
+    if cde:
+        print(json.dumps(azr, indent=4, separators=(',', ': ')))
 
+    tfrmf="125-"+tfp+"-staterm.sh"
+    tfimf="125-"+tfp+"-stateimp.sh"
+    tfrm=open(tfrmf, 'a')
+    tfim=open(tfimf, 'a')
+    print tfp,
+    count=len(azr)
+    print count
+    for i in range(0, count):
+
+        name=azr[i]["name"]
+        #loc=azr[i]["location"]
+        id=azr[i]["id"]
+        rg=id.split("/")[4].replace(".","-")
+
+        if crg is not None:
+            if rg.lower() != crg.lower():
+                continue  # back to for
+        
+        rname=name.replace(".","-")
+        prefix=tfp+"."+rg+'__'+rname
+        #print prefix
+        rfilename=prefix+".tf"
+        fr=open(rfilename, 'w')
+        fr.write(az2tfmess)
+        fr.write('resource ' + tfp + ' ' + rg + '__' + rname + ' {\n')
+        fr.write('\t name = "' + name + '"\n')
+        #fr.write('\t location = "'+ loc + '"\n')
+        fr.write('\t resource_group_name = "'+ rg + '"\n')
+
+        trm=azr[i]["properties"]["trafficRoutingMethod"]
+        fr.write('\t traffic_routing_method = "' +  trm + '"\n')
+        ps=azr[i]["properties"]["profileStatus"]
+        fr.write('\t profile_status = "' + ps + '"\n') 
+      
+        dnsc=azr[i]["properties"]["dnsConfig"]
+        monc=azr[i]["properties"]["monitorConfig"]
+                
+
+# dns_config block
+
+        rn=azr[i]["properties"]["dnsConfig"]["relativeName"]
+        ttl=azr[i]["properties"]["dnsConfig"]["ttl"]
+        
+        if ttl == 0: 
+            ttl=30
+        
+        fr.write('\t dns_config { \n')
+        fr.write('\t\t relative_name = "' + rn + '"\n')
+        #TF bug returning 0
+        fr.write('\t\t ttl  = "' + str(ttl) + '"\n')
+        fr.write('\t} \n')
+        
+# monitor_config block
+
+        prot=azr[i]["properties"]["monitorConfig"]["protocol"]
+        port=azr[i]["properties"]["monitorConfig"]["port"]
+
+        fr.write('\t monitor_config { \n')
+        fr.write('\t\t protocol = "' + prot + '"\n')
+        fr.write('\t\t port  = "' + str(port) + '"\n')
+        try:
+            path=azr[i]["properties"]["monitorConfig"]["path"]
+            fr.write('\t\t path  = "' + path + '"\n')
+        except KeyError:
+            pass
+        fr.write('\t} \n')  
+        
+
+
+# tags block       
+        try:
+            mtags=azr[i]["tags"]
+            fr.write('tags { \n')
+            for key in mtags.keys():
+                tval=mtags[key]
+                fr.write('\t "' + key + '"="' + tval + '"\n')
+            fr.write('}\n')
+        except KeyError:
+            pass
+
+        fr.write('}\n') 
+        fr.close()   # close .tf file
+
+        if cde:
+            with open(rfilename) as f: 
+                print f.read()
+
+        tfrm.write('terraform state rm '+tfp+'.'+rg+'__'+rname + '\n')
+
+        tfim.write('echo "importing ' + str(i) + ' of ' + str(count-1) + '"' + '\n')
+        tfcomm='terraform import '+tfp+'.'+rg+'__'+rname+' '+id+'\n'
+        tfim.write(tfcomm)  
+
+    # end for i loop
+
+    tfrm.close()
+    tfim.close()
+#end traffic manager profile
 
 
 # ******************************************************************************************
