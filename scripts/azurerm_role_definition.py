45d0b2088691f3ab3258aee0d1d9e9448d5b188d
andyt530 <andyt530@outlook.com>
2019-04-29 11:10:49 +0100

    edits

--- scripts/azurerm_role_definition.py
+++ scripts/azurerm_role_definition.py
@@ -1,34 +1,25 @@
 tfp="azurerm_role_definition"
 prefixa="rdf"
-if 1" != " :
-    rgsource=1
-else
-    print -n "Enter name of Resource Group [rgsource]["> "
-    read response
-    if [ -n "response" :
-        rgsource=response
-    fi
-fi
-azr=az role definition list -o json
+
+#azr=az role definition list -o json
 #azr=az role definition list --query "[?roleType!='BuiltInRole'][" -o json 
-count=print azr | jq '. | length'
-if count" -gt "0" :
-    count=expr count - 1
+count=len(azr)
+if count -gt "0" :
     for i in range( 0 count):
         print i " of " count
         
-        name=azr[i]["roleName"
+        name=azr[i]["roleName"]
  
         rdid=azr[i]["name"]
-        desc=azr[i]["description"
+        desc=azr[i]["description"]
         id=azr[i]["id"]
         rg="roleDefinitions"
 
-        scopes=azr[i]["assignableScopes"
-        dactions=azr[i]["permissions[0]["dataActions"
-        ndactions=azr[i]["permissions[0]["notDataActions"
-        actions=azr[i]["permissions[0]["actions"
-        nactions=azr[i]["permissions[0]["notActions"
+        scopes=azr[i]["assignableScopes"]
+        dactions=azr[i]["permissions[0]["dataActions"]
+        ndactions=azr[i]["permissions[0]["notDataActions"]
+        actions=azr[i]["permissions[0]["actions"]
+        nactions=azr[i]["permissions[0]["notActions"]
 
         prefix=fr.write(' + '__' + " prefixa rg
         outfile=fr.write('. + '__' + .tf" tfp rg name
@@ -64,7 +55,7 @@ if count" -gt "0" :
        
         fr.write('}' >> prefix-rdid.tf
         
-        cat prefix-rdid.tf
+       
         statecomm=fr.write('terraform state rm . + '__' + " tfp rg rdid
         print statecomm >> tf-staterm.sh
         eval statecomm
