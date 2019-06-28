be40c6c4ed183fe6e0599fca27ae26aa2afc4950
andyt530 <andyt530@outlook.com>
2019-06-28 13:36:59 +0100

    runbook update

--- runbook/az2tf-runbook.py
+++ runbook/az2tf-runbook.py
@@ -198,7 +198,8 @@ def azurerm_management_lock(crf,cde,crg,headers,requests,sub,json,az2tfmess):
             fr.write('\t lock_level = "'+ level + '"\n')   
             
             try:
-                notes=azr[j]["properties"]["notes"]                
+                notes=azr[j]["properties"]["notes"]      
+                notes=notes.encode('ascii', 'ignore')          
                 fr.write('\t notes = "'+ notes + '"\n') 
             except KeyError:
                 pass
@@ -5685,6 +5686,338 @@ def azurerm_image(crf,cde,crg,headers,requests,sub,json,az2tfmess):
     #end stub
  
 #
+# azurerm_shared_image_gallery
+#
+# azurerm_snapshot
+def azurerm_shared_image_gallery(crf,cde,crg,headers,requests,sub,json,az2tfmess):
+    tfp="azurerm_shared_image_gallery"
+    tcode="341-"
+    azr=""
+    #cde=False
+    if crf in tfp:
+    # REST or cli
+        # print "REST snapshot"
+        url="https://management.azure.com/subscriptions/" + sub + "/providers/Microsoft.Compute/galleries"
+        params = {'api-version': '2019-03-01'}
+        r = requests.get(url, headers=headers, params=params)
+        azr= r.json()["value"]
+
+
+        tfrmf=tcode+tfp+"-staterm.sh"
+        tfimf=tcode+tfp+"-stateimp.sh"
+        tfrm=open(tfrmf, 'a')
+        tfim=open(tfimf, 'a')
+        print "# " + tfp,
+        count=len(azr)
+        print count
+        for i in range(0, count):
+
+            name=azr[i]["name"]
+            loc=azr[i]["location"]
+            id=azr[i]["id"]
+            rg=id.split("/")[4].replace(".","-").lower()
+            rgs=id.split("/")[4]
+            if crg is not None:
+                if rgs.lower() != crg.lower():
+                    continue  # back to for
+            if cde:
+                print(json.dumps(azr[i], indent=4, separators=(',', ': ')))
+            
+            rname=name.replace(".","-")
+            prefix=tfp+"."+rg+'__'+rname
+            #print prefix
+            rfilename=prefix+".tf"
+            fr=open(rfilename, 'w')
+            fr.write(az2tfmess)
+            fr.write('resource ' + tfp + ' ' + rg + '__' + rname + ' {\n')
+            fr.write('\t name = "' + name + '"\n')
+            fr.write('\t location = "'+ loc + '"\n')
+            fr.write('\t resource_group_name = "'+ rgs + '"\n')
+
+
+            try:
+                co=azr[i]["properties"]["description"]
+                fr.write('\t description = "' +  co + '"\n')
+            except KeyError:
+                pass
+
+
+
+
+    # tags block       
+            try:
+                mtags=azr[i]["tags"]
+                fr.write('tags = { \n')
+                for key in mtags.keys():
+                    tval=mtags[key]
+                    fr.write('\t "' + key + '"="' + tval + '"\n')
+                fr.write('}\n')
+            except KeyError:
+                pass
+
+            fr.write('}\n') 
+            fr.close()   # close .tf file
+
+            if cde:
+                with open(rfilename) as f: 
+                    print f.read()
+
+            tfrm.write('terraform state rm '+tfp+'.'+rg+'__'+rname + '\n')
+
+            tfim.write('echo "importing ' + str(i) + ' of ' + str(count-1) + '"' + '\n')
+            tfcomm='terraform import '+tfp+'.'+rg+'__'+rname+' '+id+'\n'
+            tfim.write(tfcomm)  
+
+        # end for i loop
+
+        tfrm.close()
+        tfim.close()
+    #end stub
+ 
+#
+# azurerm_shared_image
+#
+# azurerm_snapshot
+def azurerm_shared_image(crf,cde,crg,headers,requests,sub,json,az2tfmess):
+    tfp="azurerm_shared_image"
+    tcode="342-"
+    azr=""
+    #cde=False
+    if crf in tfp:
+    # REST or cli
+        # print "REST snapshot"
+        url="https://management.azure.com/subscriptions/" + sub + "/providers/Microsoft.Compute/galleries"
+        params = {'api-version': '2019-03-01'}
+        r = requests.get(url, headers=headers, params=params)
+        azr= r.json()["value"]
+
+    
+        tfrmf=tcode+tfp+"-staterm.sh"
+        tfimf=tcode+tfp+"-stateimp.sh"
+        tfrm=open(tfrmf, 'a')
+        tfim=open(tfimf, 'a')
+        
+        count=len(azr)
+        for i in range(0, count):
+
+            gname=azr[i]["name"]
+            gloc=azr[i]["location"]
+            id=azr[i]["id"]
+            rg=id.split("/")[4].replace(".","-").lower()
+            rgs=id.split("/")[4]
+            if crg is not None:
+                if rgs.lower() != crg.lower():
+                    continue  # back to for
+            if cde:
+                print(json.dumps(azr[i], indent=4, separators=(',', ': ')))
+            
+        
+
+
+            url="https://management.azure.com/subscriptions/" + sub + "/resourceGroups/"+rg+"/providers/Microsoft.Compute/galleries/"+gname+"/images"
+            params = {'api-version': '2019-03-01'}
+            r = requests.get(url, headers=headers, params=params)
+            azr2= r.json()["value"]
+            jcount=len(azr2)
+            print "# " + tfp,
+            print jcount
+            for j in range(0, jcount):
+                
+                if cde:
+                    print(json.dumps(azr2[j], indent=4, separators=(',', ': ')))
+                name=azr2[j]["name"]
+                loc=azr2[j]["location"]
+                id=azr2[j]["id"]
+
+                rname=name.replace(".","-")
+                prefix=tfp+"."+rg+'__'+gname+'__'+rname
+                #print prefix
+                rfilename=prefix+".tf"
+                fr=open(rfilename, 'w')
+                fr.write(az2tfmess)
+                fr.write('resource ' + tfp + ' ' + rg + '__' + gname + '__' + rname + ' {\n')
+                fr.write('\t name = "' + name + '"\n')
+                fr.write('\t location = "'+ loc + '"\n')
+                fr.write('\t resource_group_name = "'+ rg + '"\n')
+                fr.write('\t gallery_name = "'+ gname + '"\n')
+
+                try:
+                    ost=azr2[j]["properties"]["osType"]
+                    fr.write('\t os_type = "' +  ost + '"\n')
+                except KeyError:
+                    pass
+                sku=azr2[j]["properties"]["identifier"]["sku"]
+                pub=azr2[j]["properties"]["identifier"]["publisher"]
+                off=azr2[j]["properties"]["identifier"]["offer"]
+                fr.write('\t identifier { \n')
+                fr.write('\t\t sku ="'+sku+'"\n')
+                fr.write('\t\t publisher ="'+pub+'"\n')
+                fr.write('\t\t offer ="'+off+'"\n')
+                fr.write('\t} \n')
+
+
+        # tags block       
+                try:
+                    mtags=azr2[j]["tags"]
+                    fr.write('tags = { \n')
+                    for key in mtags.keys():
+                        tval=mtags[key]
+                        fr.write('\t "' + key + '"="' + tval + '"\n')
+                    fr.write('}\n')
+                except KeyError:
+                    pass
+
+                fr.write('}\n') 
+                fr.close()   # close .tf file
+
+                if cde:
+                    with open(rfilename) as f: 
+                        print f.read()
+
+                tfrm.write('terraform state rm '+tfp+'.'+rg+'__'+gname+'__'+rname + '\n')
+
+                tfim.write('echo "importing ' + str(j) + ' of ' + str(jcount-1) + '"' + '\n')
+                tfcomm='terraform import '+tfp+'.'+rg+'__'+gname+'__'+rname+' '+id+'\n'
+                tfim.write(tfcomm)  
+
+        # end for i loop
+
+        tfrm.close()
+        tfim.close()
+    #end stub
+ 
+#
+# azurerm_shared_image_version
+#
+# azurerm_snapshot
+def azurerm_shared_image_version(crf,cde,crg,headers,requests,sub,json,az2tfmess):
+    tfp="azurerm_shared_image_version"
+    tcode="343-"
+    azr=""
+    #cde=False
+    if crf in tfp:
+    # REST or cli
+        # print "REST snapshot"
+        url="https://management.azure.com/subscriptions/" + sub + "/providers/Microsoft.Compute/galleries"
+        params = {'api-version': '2019-03-01'}
+        r = requests.get(url, headers=headers, params=params)
+        azr= r.json()["value"]
+
+    
+        tfrmf=tcode+tfp+"-staterm.sh"
+        tfimf=tcode+tfp+"-stateimp.sh"
+        tfrm=open(tfrmf, 'a')
+        tfim=open(tfimf, 'a')
+        
+        count=len(azr)
+  
+        for i in range(0, count):
+
+            gname=azr[i]["name"]
+            loc=azr[i]["location"]
+            id=azr[i]["id"]
+            rg=id.split("/")[4].replace(".","-").lower()
+            rgs=id.split("/")[4]
+            if crg is not None:
+                if rgs.lower() != crg.lower():
+                    continue  # back to for
+        
+            url="https://management.azure.com/subscriptions/" + sub + "/resourceGroups/"+rg+"/providers/Microsoft.Compute/galleries/"+gname+"/images"
+            params = {'api-version': '2019-03-01'}
+            r = requests.get(url, headers=headers, params=params)
+            azr2= r.json()["value"]
+            jcount=len(azr2)
+           
+            for j in range(0, jcount):
+
+                iname=azr2[j]["name"]
+                rname=iname.replace(".","-")
+
+                url="https://management.azure.com/subscriptions/" + sub + "/resourceGroups/"+rg+"/providers/Microsoft.Compute/galleries/"+gname+"/images/"+iname+"/versions"
+                params = {'api-version': '2019-03-01'}
+                r = requests.get(url, headers=headers, params=params)
+                azr3= r.json()["value"]
+                kcount=len(azr3)
+                print "# " + tfp,
+                print kcount
+                for k in range(0, kcount):
+
+                    if cde:
+                        
+                        print(json.dumps(azr3[k], indent=4, separators=(',', ': ')))
+
+                    name=azr3[k]["name"]
+                    loc=azr3[k]["location"]
+                    id=azr3[k]["id"]
+                    rg=id.split("/")[4].replace(".","-").lower()
+                    rgs=id.split("/")[4]
+                    rname = name.replace(".", "-")
+                    prefix=tfp+"."+rg+'__'+gname+'__'+iname+'__'+rname
+                    
+                    #print prefix
+                    rfilename=prefix+".tf"
+                    fr=open(rfilename, 'w')
+                    fr.write(az2tfmess)
+                  
+                    fr.write('resource ' + tfp + ' ' + rg + '__' + gname + '__' + iname + '__' + rname + ' {\n')  
+                    fr.write('\t name = "' + name + '"\n')
+                    fr.write('\t location = "'+ loc + '"\n')
+                    fr.write('\t resource_group_name = "'+ rgs + '"\n')
+                    fr.write('\t gallery_name = "'+ gname + '"\n')
+                    fr.write('\t image_name = "'+ iname + '"\n')
+                    mid=azr3[k]["properties"]["publishingProfile"]["source"]["managedImage"]["id"]  
+                    fr.write('\t managed_image_id = "'+ mid + '"\n')
+                    try:
+                        tr=azr3[k]["properties"]["publishingProfile"]["targetRegions"]
+                        tcount=len(tr)
+                        
+                        for t in range(0, tcount):
+                            tnam=tr[t]["name"]
+                            rrc=tr[t]["regionalReplicaCount"]
+                            fr.write('\t target_region { \n')
+                            fr.write('\t\t name ="'+tnam+'"\n') 
+                            fr.write('\t\t regional_replica_count ="'+str(rrc)+'"\n')  
+                            fr.write('\t} \n')
+
+
+
+                    except KeyError:
+                        pass
+
+
+
+            # tags block       
+                    try:
+                        mtags=azr3[k]["tags"]
+                        fr.write('tags = { \n')
+                        for key in mtags.keys():
+                            tval=mtags[key]
+                            fr.write('\t "' + key + '"="' + tval + '"\n')
+                        fr.write('}\n')
+                    except KeyError:
+                        pass
+
+                    fr.write('}\n') 
+                    fr.close()   # close .tf file
+
+                    if cde:
+                        with open(rfilename) as f: 
+                            print f.read()
+
+                   
+                    tfrm.write('terraform state rm '+tfp+'.'+rg+'__'+gname+'__'+iname+'__'+rname + '\n')
+                    tfim.write('echo "importing ' + str(k) + ' of ' + str(kcount-1) + '"' + '\n')
+                    tfcomm='terraform import '+tfp+'.'+rg+'__'+gname+'__'+iname+'__'+rname+' '+id+'\n'
+                  
+                    tfim.write(tfcomm)  
+
+        # end for i loop
+
+        tfrm.close()
+        tfim.close()
+    #end stub
+ 
+#
 # azurerm_snapshot
 #
 # azurerm_snapshot
@@ -7481,6 +7814,127 @@ def azurerm_policy_definition(crf,cde,crg,headers,requests,sub,json,az2tfmess):
         tfim.close()
     #end stub 
 #
+# azurerm_policy_set_definition
+#
+import ast
+
+def azurerm_policy_set_definition(crf, cde, crg, headers, requests, sub, json, az2tfmess):
+    tfp = "azurerm_policy_set_definition"
+    
+    azr = ""
+    if crf in tfp:
+        # REST or cli
+        #print "REST Pol Defn"
+        url = "https://management.azure.com/subscriptions/" + sub + \
+            "/providers/Microsoft.Authorization/policySetDefinitions"
+        params = {'api-version': '2019-01-01'}
+        r = requests.get(url, headers=headers, params=params)
+        azr = r.json()["value"]
+
+        tfrmf = "008-"+tfp+"-staterm.sh"
+        tfimf = "008-"+tfp+"-stateimp.sh"
+        tfrm = open(tfrmf, 'a')
+        tfim = open(tfimf, 'a')
+        print "# " + tfp,
+        count = len(azr)
+        print count
+        for i in range(0, count):
+
+            name = azr[i]["name"]
+            # loc=azr[i]["location"]
+            id = azr[i]["id"]
+            rg = "policysetdefinitions"
+            rgs = id.split("/")[4]
+
+            if crg is not None:
+                if rg.lower() != crg.lower():
+                    continue  # back to for
+
+            rname = name.replace(".", "-")
+            prefix = tfp+"."+rg+'__'+rname
+            #print prefix
+            pt = azr[i]["properties"]["policyType"]
+            if pt != "BuiltIn" :
+                if cde:
+                    print(json.dumps(azr[i], indent=4, separators=(',', ': ')))
+                rfilename = prefix+".tf"
+                fr = open(rfilename, 'w')
+                fr.write(az2tfmess)
+                fr.write('resource ' + tfp + ' ' + rg + '__' + rname + ' {\n')
+                fr.write('\t name = "' + name + '"\n')
+                    #fr.write('\t location = "'+ loc + '"\n')
+                    #fr.write('\t resource_group_name = "'+ rgs + '"\n')
+
+                rdid = azr[i]["name"]            
+                pt = azr[i]["properties"]["policyType"]
+                rg = "policySetDefinitions"
+
+
+                try:
+                    dname = azr[i]["properties"]["displayName"]
+                    fr.write('display_name =  "'+dname+'"\n')
+                except KeyError:
+                    fr.write('display_name = ""\n')
+                    pass
+
+                fr.write('policy_type = "' + pt + '"\n') 
+                #fr.write('mode = "' + mode + '"\n')
+                try:
+                    desc = azr[i]["properties"]["description"]
+                    desc=desc.replace('\\','\\\\')
+                    fr.write('description =  "'+desc + '"\n') 
+                except KeyError:
+                    pass
+
+                #print(json.dumps(azr[i]["properties"]["metadata"], indent=4, separators=(',', ': ')))
+
+
+                try:
+                    params = azr[i]["properties"]["parameters"]          
+                    pl = len(params)
+                    if pl > 0:
+                        fr.write('parameters = jsonencode( \n')
+                        fr.write(json.dumps(params))
+                        fr.write(') \n')
+                except KeyError:
+                    pass
+
+
+                try:
+                    pdef = str(ast.literal_eval(json.dumps(azr[i]["properties"]["policyDefinitions"])))
+                    fr.write('policy_definitions= jsonencode( \n')
+                    fr.write(json.dumps(pdef))
+                    fr.write(') \n')
+                    #pcount=len(pdef)             
+                    #for p in range(0, pcount):
+                    #    fr.write('policy_definitions= jsonencode( \n')
+                    #    fr.write(json.dumps(pdef[p]))
+                    #    fr.write(') \n')
+                    
+                except KeyError:
+                    pass
+        
+
+                fr.write('}\n')
+                fr.close()   # close .tf file
+
+                if cde:
+                    with open(rfilename) as f:
+                        print f.read()
+
+                tfrm.write('terraform state rm '+tfp+'.'+rg+'__'+rname + '\n')
+
+                tfim.write('echo "importing ' + str(i) + ' of ' + str(count-1) + '"' + '\n')
+                tfcomm = 'terraform import '+tfp+'.'+rg+'__'+rname+' '+id+'\n'
+                tfim.write(tfcomm)
+
+        # end for i loop
+
+        tfrm.close()
+        tfim.close()
+    # end stub
+ 
+#
 # azurerm_policy_assignment
 #
 
@@ -7880,7 +8334,7 @@ azurerm_virtual_machine(crf,cde,crg,headers,requests,sub,json,az2tfmess)
 # 290_azurerm_virtual_machine_extension
 azurerm_virtual_machine_extension(crf,cde,crg,headers,requests,sub,json,az2tfmess)
 # 295_azurerm_virtual_machine_scale_set
-#azurerm_virtual_machine_scale_set(crf,cde,crg,headers,requests,sub,json,az2tfmess)
+azurerm_virtual_machine_scale_set(crf,cde,crg,headers,requests,sub,json,az2tfmess)
 # 310_azurerm_automation_account
 azurerm_automation_account(crf,cde,crg,headers,requests,sub,json,az2tfmess)
 # 320_azurerm_log_analytics_workspace
@@ -7889,6 +8343,11 @@ azurerm_log_analytics_workspace(crf,cde,crg,headers,requests,sub,json,az2tfmess)
 azurerm_log_analytics_solution(crf,cde,crg,headers,requests,sub,json,az2tfmess)
 # 340_azurerm_image
 azurerm_image(crf,cde,crg,headers,requests,sub,json,az2tfmess)
+
+# 341_azurerm_shared_image_gallery
+azurerm_shared_image_gallery(crf,cde,crg,headers,requests,sub,json,az2tfmess)
+azurerm_shared_image(crf,cde,crg,headers,requests,sub,json,az2tfmess)
+azurerm_shared_image_version(crf,cde,crg,headers,requests,sub,json,az2tfmess)
 # 350_azurerm_snapshot
 azurerm_snapshot(crf,cde,crg,headers,requests,sub,json,az2tfmess)
 # 360_azurerm_network_watcher
